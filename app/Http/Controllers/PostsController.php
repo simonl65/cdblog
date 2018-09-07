25e985ce65adc2830886f59e83e6b3acf34f7999
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-07 15:41:48 +0100

    Post has $fillable so we can: Post::create(request(['title', 'body']));

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -39,17 +39,8 @@ class PostsController extends Controller
         // Create a new blog post using the request data:
         $post = new Post;
 
-        $post->title = request('title');
-        $post->body = request('body');
-
         // Save post to database:
-        $post->save();
-
-        // The following achieves the same, but we need to watch for mass-submit
-        // issue which requires us to specify (in Post.php model) which fields are
-        // OK to submit (using: protected $fillable = ['title', 'body'];)
-        //
-        // Post::create( request[ 'title', 'body' ] );
+        Post::create( request( ['title', 'body'] ) );
 
         // Redirect to the home page:
         return redirect('/posts');
