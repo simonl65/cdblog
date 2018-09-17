2ce5a6741dd0a254f1c43c7ef2c407dfab1dd264
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-17 10:06:17 +0100

    Fixes issue where guest attempts visit to /posts/create but got error instead of /login.

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::get('/tasks/{task}', 'TasksController@show');
 Route::get('/register',     'RegistrationController@create');
 Route::post('/register',    'RegistrationController@store');
 
-Route::get('/login',    'SessionsController@create');
+Route::get('/login',    'SessionsController@create')->name('login');
 Route::post('/login',   'SessionsController@store');
 Route::get('/logout',   'SessionsController@destroy');
