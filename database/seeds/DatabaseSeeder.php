c57f9b92f6ab5c4be754064c262ac0ac67442468
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-09-18 13:01:41 +0100

    Updated database seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -72,6 +72,27 @@ class DatabaseSeeder extends Seeder
             'created_at' => '2018-09-06 14:27',
             'updated_at' => '2018-09-06 14:27',
         ]);
+        DB::table('posts')->insert([
+            'user_id' => 2,
+            'title' => 'January',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime asperiores beatae suscipit iste impedit quae necessitatibus sed reprehenderit accusantium hic sunt, dolor autem veritatis odio officia, nulla enim placeat aliquid modi totam praesentium temporibus libero! Enim ut itaque, dolor similique mollitia quae facilis nostrum tenetur pariatur soluta autem explicabo, adipisci harum quisquam porro sunt quam beatae accusamus, maxime praesentium consequatur reiciendis quidem repellat! Quo perferendis tempora autem itaque neque facere commodi dolor dolores ab, officiis culpa deleniti libero provident quia ex nemo, excepturi ut tempore atque modi aliquam ad ipsam nisi soluta? Dolores eaque ut, quisquam doloremque veniam accusamus et.',
+            'created_at' => '2018-01-06 19:00',
+            'updated_at' => '2018-01-06 19:00',
+        ]);
+        DB::table('posts')->insert([
+            'user_id' => 2,
+            'title' => 'February',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus dolore est fugit dignissimos necessitatibus inventore a totam provident aut fuga rem, accusantium suscipit blanditiis incidunt adipisci quas quaerat molestiae laborum impedit corrupti! Quaerat nihil eum, amet distinctio repellendus est iure?',
+            'created_at' => '2018-02-06 12:35',
+            'updated_at' => '2018-02-06 12:35',
+        ]);
+        DB::table('posts')->insert([
+            'user_id' => 1,
+            'title' => 'November',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, totam voluptatibus laboriosam corrupti pariatur aliquam magni! Nemo obcaecati eum nam? Molestiae pariatur cum fugit vel aperiam eos harum veniam officiis, architecto magni impedit maiores explicabo ipsa, quam repellat doloribus, facilis non necessitatibus voluptas expedita voluptate.',
+            'created_at' => '2017-11-06 13:30',
+            'updated_at' => '2017-11-06 13:30',
+        ]);
 
         /**
          * Comments
@@ -97,5 +118,30 @@ class DatabaseSeeder extends Seeder
             'created_at' => '2018-09-06 16:01',
             'updated_at' => '2018-09-06 16:01',
         ]);
+
+
+        /**
+         * Tags & post_tag mappings:
+         */
+        DB::table('tags')->insert([
+            'name' => 'personal',
+            'created_at' => '2018-09-18 12:50',
+            'updated_at' => '2018-09-18 12:50',
+        ]);
+        DB::table('tags')->insert([
+            'name' => 'php',
+            'created_at' => '2018-09-18 12:55',
+            'updated_at' => '2018-09-18 12:55',
+        ]);
+
+        DB::table('post_tag')->insert([
+            'post_id' => 1,
+            'tag_id' => 1
+        ]);
+        DB::table('post_tag')->insert([
+            'post_id' => 1,
+            'tag_id' => 2
+        ]);
+
     }
 }
